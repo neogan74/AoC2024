84869983433739fd4da9d61ef264ea7de80a72ae
Andrei Gushchin <neogan74@gmail.com>
2024-12-24 21:49:35 +0300

    app

--- hangman/internal/app/app.go
+++ hangman/internal/app/app.go
@@ -1,5 +1,21 @@
 package app
 
-type App strunct {
+import (
+	"github.com/go-openapi/runtime/logger"
+	"honnef.co/go/tools/config"
+)
+
+type Game struct {
+	Log    logger.Logger
+	Config *config.Config
+}
+
+func NewGame(log logger.Logger, cfg *config.Config) *Game {
+	return &Game{
+		Log:    log,
+		Config: cfg,
+	}
+}
+func (g *Game) Run() {
 
 }
